ce46f4a2c9b01b9c4c3dbce77626c7c01a2aa902
khoavd001 <72806367+khoavd001@users.noreply.github.com>
2022-09-24 17:54:13 +0700

    file style

---
+++ Phần mềm vàng bạc trang sức/BUS(interact file)/Add_Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phần_mềm_vàng_bạc_trang_sức.BUS_interact_file_
+{
+    internal class Add_Customer
+    {
+    }
+}
---
+++ Phần mềm vàng bạc trang sức/DTO(build object class)/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phần_mềm_vàng_bạc_trang_sức.DTO_build_object_class_
+{
+    internal class Customer
+    {
+    }
+}
